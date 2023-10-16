f17cc35cb82d5e03f0ac303801638567813b689f
sdevkota007 <sdevkota007@gmail.com>
2023-10-16 11:56:13 -0400

    Delete 10_multipleTexsMultilpleTexUnits directory

--- 10_multipleTexsMultilpleTexUnits/utils.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-
-def load_image(filename, format="RGB", flip=False):
-    img = pg.image.load(filename)
-    img_data = pg.image.tobytes(img, format, flip)
-    w, h = img.get_size()
-    return img_data, w, h
